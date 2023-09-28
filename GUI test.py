22bee153b499e0f59b1260e181040aa97e9f72f7
YatoKa7 <harut.m1999@gmail.com>
2023-09-28 13:45:53 +0400

    Changes and new project

---
+++ GUI test.py
@@ -0,0 +1,30 @@
+import PySimpleGUI as sg
+import random
+
+def update():
+    r = random.randint(1,10) #+
+
+    text_elem = window['-text-'] #
+
+    text_elem.update("Your random numbers: {}".format(r))#
+
+
+    
+    layout = [[sg.Button('Click!', enable_events=True, key='-FUNCTION-', font='Helvetica 16')],
+              
+        [sg.Text('Your numbers', size=(25, 1), key='-text-', font='Helvetica 16')]] #
+
+    window = sg.Window('Randomizer', layout, size=(350,100))
+
+    while True:
+
+        event, values = window.read()
+
+        if event in (sg.WIN_CLOSED, 'Hajox'):
+            break
+
+        if event == '-FUNCTION-':
+
+            update()
+
+            window.close()
