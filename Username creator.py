41ef7cd42e0a1bb7ee696196b8f39e0b38b74931
YatoKa7 <146266450+YatoKa7@users.noreply.github.com>
2023-09-28 15:31:18 +0400

    new file:   Username creator.py

---
+++ Username creator.py
@@ -0,0 +1,27 @@
+a = input("Name: ")
+b = input("Surname: ")
+
+#a = ("Harutyun")
+#b = ("Martirosyan")
+#print user
+
+print("Հարգելի գործընկերներ,\n\nԿատարված է՝")
+
+print('User: '+ a,b)
+
+#print Username
+
+#print(a.lower())
+
+#a = a[0]
+a = a.lower()
+b = b.lower()
+print(("Username: ")+a[0]+(".")+b)
+
+c = a[0]+(".")+b
+
+print("E-Mail:"+c+("@rtarmenia.am"))
+
+print("Password: Aa123456")
+
+#print(a.lower())
